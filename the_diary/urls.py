1f819c10c92fd7794945243e153ca6b0e552a5b3
Ivan Veremeichik <ivan.veremeichik@gmail.com>
2022-03-26 19:54:49 +0300

    Iteration 2 Created detailed publication page. Added links to them and back to home

--- the_diary/urls.py
+++ the_diary/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Home, PublicationDetailedView
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    # path('', views.home, name="home"),
+    path('', Home.as_view(), name="home"),
+    path('publication/<int:pk>', PublicationDetailedView.as_view(), name="publication-detail"),
 ]
--- the_diary/views.py
+++ the_diary/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Publication
 
 
-def home(request):
-    return render(request, 'home.html', {})
+# def home(request):
+#    return render(request, 'home.html', {})
+
+class Home(ListView):
+    model = Publication
+    template_name = 'home.html'
+
+
+class PublicationDetailedView(DetailView):
+    model = Publication
+    template_name = 'details.html'
