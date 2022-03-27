9edc9235dfd93fa625e16df454bda6fd6d6ff400
Ivan Veremeichik <ivan.veremeichik@gmail.com>
2022-03-27 16:30:54 +0300

    Iteration 13 Added categories_list page

--- the_diary/views.py
+++ the_diary/views.py
@@ -5,20 +5,29 @@ from .forms import PublicationForm, EditForm
 from django.urls import reverse_lazy
 
 
-# def home(request):
-#    return render(request, 'home.html', {})
-
-
 class Home(ListView):
     model = Publication
     template_name = 'home.html'
     ordering = ['-publication_date', '-id']
+
     # ordering = ['-id']
+
+    def get_context_data(self, *args, **kwargs):
+        category_menu = Category.objects.all()
+        context = super(Home, self).get_context_data(*args, **kwargs)
+        context["category_menu"] = category_menu
+        return context
 
 
 class PublicationDetailedView(DetailView):
     model = Publication
     template_name = 'details.html'
+
+    def get_context_data(self, *args, **kwargs):
+        category_menu = Category.objects.all()
+        context = super(PublicationDetailedView, self).get_context_data(*args, **kwargs)
+        context["category_menu"] = category_menu
+        return context
 
 
 class AddPublicationView(CreateView):
@@ -49,3 +58,8 @@ def category_view(request, categories):
     category_publications = Publication.objects.filter(category=categories.replace('-', ' '))
     return render(request, 'categories.html',
                   {'categories': categories.title().replace('-', ' '), 'category_publications': category_publications})
+
+
+def category_list_view(request):
+    category_menu_list = Category.objects.all()
+    return render(request, 'categories_list.html', {'category_menu_list': category_menu_list})
