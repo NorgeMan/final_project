8299d0cf3546450d383f56b528bbe7f8761efd25
Ivan Veremeichik <ivan.veremeichik@gmail.com>
2022-03-27 23:32:30 +0300

    Iteration 19 Added edit user profile form and its styles

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import UserSignUpView
+from .views import UserSignUpView, UserEditView
 
 urlpatterns = [
     path('sign-up/', UserSignUpView.as_view(), name='sign-up'),
-
+    path('edit_profile/', UserEditView.as_view(), name='edit-profile'),
 ]
