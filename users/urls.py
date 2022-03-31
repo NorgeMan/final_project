1677230eb350c1bdeaa5287767dbc2e97eef956d
Ivan Veremeichik <ivan.veremeichik@gmail.com>
2022-04-01 02:04:49 +0300

    Iteration 25 Created blog profile page

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserSignUpView, UserEditView, ChangePasswordsView
+from .views import UserSignUpView, UserEditView, ChangePasswordsView, ShowProfilePageView
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -9,4 +9,5 @@ urlpatterns = [
     # path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change_password.html')),
     path('password/', ChangePasswordsView.as_view(template_name='registration/change_password.html')),
     path('password_success', views.password_success, name="password_success"),
+    path('<int:pk>/profile/', ShowProfilePageView.as_view(), name='show_profile_page'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,10 +1,13 @@
 from django.contrib.auth.views import PasswordChangeView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from django.views.generic import DetailView
+
 from .forms import SignUpForm, EditProfileForm, PasswordChangesForm
+from the_diary.models import Profile
 
 
 class UserSignUpView(generic.CreateView):
@@ -31,3 +34,16 @@ class ChangePasswordsView(PasswordChangeView):
     # form_class = PasswordChangeForm
     # success_url = reverse_lazy('home')
     success_url = reverse_lazy('password_success')
+
+
+class ShowProfilePageView(DetailView):
+    model = Profile
+    template_name = 'registration/user_profile.html'
+
+    def get_context_data(self, *args, **kwargs):
+        # users = Profile.objects.all()
+        context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
+        page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
+
+        context["page_user"] = page_user
+        return context
