27163d3daa695b4730d51ba75bb7d1b7f979e48e
Ivan Veremeichik <ivan.veremeichik@gmail.com>
2022-04-01 21:52:49 +0300

    Iteration 28 Added draft version of comments

--- the_diary/admin.py
+++ the_diary/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Publication, Category, Profile
+from .models import Publication, Category, Profile, Comment
 
 admin.site.register(Publication)
 admin.site.register(Category)
 admin.site.register(Profile)
+admin.site.register(Comment)
